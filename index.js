1b84a23ce689ae31d372c745847e4846d651e575
TCahnaf <ahnafchwd164@gmail.com>
2025-09-08 22:57:31 -0400

    wrapping up

--- index.js
+++ index.js
@@ -4,15 +4,16 @@ function captureElementID(id){
 
 };
 
-const addHighlight = (id)=>{
-    captureElementID(id).classList.add("bg-green-700")
+const removeHighlights = ()=>{
+    const elements = document.getElementsByClassName("category-btn");
+    for(const element of elements){
+        element.classList.remove("bg-green-700", "text-white")
+    }
+    
 };
 
-const removeHigglight = (id) =>{
-    captureElementID(id).classList.remove("bg-green-700")
-};
 
-
+   
 const loadSpinnerOn = () =>{
     document.getElementById("spinner").classList.remove("hidden")
     document.getElementById("cards-container").classList.add("hidden")
@@ -52,14 +53,19 @@ const loadPlantCards = async()=>{
     const data = await res.json();
     displayPlantCards(data.plants)
 
+
+
 }
 
 const loadEachPlantCards = async(id)=>{
     loadSpinnerOn();
     const res = await fetch(`https://openapi.programming-hero.com/api/category/${id}`);
     const data = await res.json();
+    removeHighlights();
+    const clickBtn = document.getElementById(`btn-ctgr-${id}`)
+    clickBtn.classList.add("bg-green-700", "text-white")
     displayPlantCards(data.plants)
-
+    
 }
 
 const displayPlantDetails = plant => {
@@ -89,15 +95,15 @@ const displayPlantCards = plants => {
     allPlantCards.innerHTML = ""
     for(const plant of plants){
         plantCard = document.createElement("div")
-        plantCard.innerHTML = `<div class  = "bg-white p-4 flex flex-col justify-between h-full rounded-xl ">
-                <img class = "h-40 w-full object-cover rounded-t" src= ${plant.image} alt="">
+        plantCard.innerHTML = `<div class  = "bg-white p-4 flex flex-col justify-between h-full rounded-xl shadow-lg space-y-3">
+                <img class = "h-60 w-full object-cover rounded-t" src= ${plant.image} alt="">
                 <h2 class  = "font-bold plant-name">${plant.name}</h2>
                 <p>${plant.description}</p>
-                <div class = "flex justify-between">
-                <button onclick = "loadPlantDetails(${plant.id})">${plant.category}</button>
+                <div class = "flex justify-between items-center">
+                <button class = "bg-[#DCFCE7] rounded-2xl p-2" onclick = "loadPlantDetails(${plant.id})">${plant.category}</button>
                 <p class = "plant-price" >৳<span class = "money-value">${plant.price}</span></p>
                 </div>
-                <button class = "cart-btn pl-2 bg-[#15803D] w-11/12 rounded-xl text-center">Add to Cart</button>
+                <button class = "cart-btn px-4 py-2 bg-[#15803D] rounded-xl text-center text-white lg:h-[43px]">Add to Cart</button>
             </div>
 
            `
@@ -111,9 +117,9 @@ const displayPlantCards = plants => {
 
        
 
-                 const cartContent = captureElementID("cart-content");
+                const cartContent = captureElementID("cart-content");
                 const totalDiv = document.createElement("div")
-                 //cartContent.appendChild(totalDiv);
+            
        
         document.querySelectorAll(".cart-btn").forEach((btn) =>{
            
@@ -126,9 +132,9 @@ const displayPlantCards = plants => {
                 alert(`${parent.querySelector(".plant-name").innerText} has been added to your cart`
              );
                  cartDetails.innerHTML = `
-            <div class = "flex justify-between items-center px-4 rounded-xl bg-[#F0FDF4]">
+            <div class = "flex justify-between items-center px-2 rounded-l w-[168px] bg-[#F0FDF4]">
             <div>
-                 <h2>${parent.querySelector(".plant-name").innerText}</h2>
+                 <h2 class = "font-bold">${parent.querySelector(".plant-name").innerText}</h2>
             <h2>${parent.querySelector(".plant-price").innerHTML} x 1</h2>
            </div>
            <div> <button class = "remove-btn">X</button></div>
@@ -170,23 +176,26 @@ const displayPlantCards = plants => {
 const displayCategories = (categories)=>{
     const categoryContainer = captureElementID("category-container");
     categoryContainer.innerHTML = "";
-    categoryContainer.innerHTML = `<h2 class = "text-center font-bold">Categories</h2>
-    <button onclick = "loadPlantCards()" class = "hover:bg-green-300" >All Trees</button>
-    
+    categoryContainer.innerHTML = `<h2 class = "font-bold">Categories</h2>
+    <button onclick = "loadPlantCards()" id = "all-tree-btn " class = "text-left category-btn border-2 bg-green-700 text-white" >All Trees</button>
     `
+    
 
     categories.forEach(category => {
-        const categoryButton = document.createElement("button");
+        const categoryButtons = document.createElement("div");
         
-       categoryButton.innerHTML = `<button class = "category-btn bg-font-bold hover:bg-green-300" onclick = "loadEachPlantCards(${category.id})">${category.category_name}</button>`
-    //    categoryButton.style.backgroundColor = "lightblue";
+       categoryButtons.innerHTML = `<button id = "btn-ctgr-${category.id}" class = "category-btn whitespace-nowrap  hover:bg-green-300" onclick = "loadEachPlantCards(${category.id})">${category.category_name}</button>`
+    
         
-        categoryContainer.append(categoryButton)
+        categoryContainer.append(categoryButtons)
         
 
     })
 
 };
+
+
+
 
 loadPlantCards();
 loadCategories();
@@ -199,39 +208,3 @@ loadCategories();
 
 
 
-
-/*
-  <div class  = "bg-white p-10 rounded-xl w-[311.33px]">
-                <img src=" https://i.ibb.co.com/cSQdg7tf/mango-min.jpg" alt="">
-                <h2 class  = "font-bold">Mango Tree</h2>
-                <p>A fast-growing tropical tree that produces delicious, juicy mangoes during summer. Its dense green</p>
-                <div class = "flex justify-between">
-                <button>Fruit Tree</button>
-                <p>৳500</p>
-                </div>
-                <button class = "pl-2 bg-[#15803D] w-11/12 rounded-xl text-center">Add to Cart</button>
-            </div>
-
-            <div class  = "bg-white p-10 rounded-xl w-[311.33px]">
-                <img src=" https://i.ibb.co.com/cSQdg7tf/mango-min.jpg" alt="">
-                <h2 class  = "font-bold">Mango Tree</h2>
-                <p>A fast-growing tropical tree that produces delicious, juicy mangoes during summer. Its dense green</p>
-                <div class = "flex justify-between">
-                <button>Fruit Tree</button>
-                <p>৳500</p>
-                </div>
-                <button class = "pl-2 bg-[#15803D] w-11/12 rounded-xl text-center">Add to Cart</button>
-            </div>
-
-            <div class  = "bg-white p-10 rounded-xl w-[311.33px]">
-                <img src=" https://i.ibb.co.com/cSQdg7tf/mango-min.jpg" alt="">
-                <h2 class  = "font-bold">Mango Tree</h2>
-                <p>A fast-growing tropical tree that produces delicious, juicy mangoes during summer. Its dense green</p>
-                <div class = "flex justify-between">
-                <button>Fruit Tree</button>
-                <p>৳500</p>
-                </div>
-                <button class = "pl-2 bg-[#15803D] w-11/12 rounded-xl text-center">Add to Cart</button>
-            </div>
-
-*/
